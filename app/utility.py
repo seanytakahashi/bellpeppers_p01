64b8a7787aef004d16c35c34f7d0ef6696d680ba
James Lei <leijames325@gmail.com>
2025-12-05 10:29:06 -0500

    Added general_query

--- app/utility.py
+++ app/utility.py
@@ -9,9 +9,17 @@ import sqlite3
 DB_FILE = "data.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False)
 
+# data: {"key": value}
 def insert_query(table, data):
     c = db.cursor()
     placeholder = ["?"] * len(data)
-    c.execute(f"INSERT INTO {table} {tuple(data.keys())} VALUES ({', '.join(placeholder)}) RETURNING *;", tuple(data.values()))
+    c.execute(f"INSERT INTO {table} {tuple(data.keys())} VALUES ({', '.join(placeholder)});", tuple(data.values()))
+    c.close()
+    db.commit()
+
+# parameters: [val1, val2]
+def general_query(query_string, parameters=()):
+    c = db.cursor()
+    c.execute(query_string, parameters)
     c.close()
     db.commit()
