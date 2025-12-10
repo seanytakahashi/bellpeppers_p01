d8d53a2b1c2683f27659ad6322205439ae7889cd
Eviss W <ewu60@stuy.edu>
2025-12-10 10:31:45 -0500

    chances travel

--- app/travel.py
+++ app/travel.py
@@ -3,8 +3,6 @@ import urllib.parse
 import json
 from flask import Flask
 
-curKey = 'GER'
-
 def getCommon(key):
     url = "https://restcountries.com/v3.1/alpha/" + key
     with urllib.request.urlopen(url) as response:
@@ -12,6 +10,14 @@ def getCommon(key):
     common = data[0]['name']['common']
     #print(common)
     return(common)
+
+def getPop(key):
+    url = "https://restcountries.com/v3.1/alpha/" + key
+    with urllib.request.urlopen(url) as response:
+        data = json.loads(response.read())
+    pop = data[0]['maps']['population']
+    #print(pop)
+    return(pop)
 
 def getList():
     url = "https://restcountries.com/v3.1/alpha/" + curKey
@@ -28,7 +34,27 @@ def getCommonList():
     #print(list)
     return list
 
-getCommonList()
+def getPopList():
+    list = []
+    for country in getList():
+        list.append(getPop(country));
+    #print(list)
+    return list
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+def make_numbers(p):
+    nums = []
+    current = p
+
+    for times in range(15):
+        current = (current * 1103515245 + 12345) % 2**31
+        nums.append((current % 1000000) / 1000000)
+    total = sum(nums)
+    scale = 100 / total
+    return [n * scale for n in nums]
+
+def getChanceList():
+    list = []
+    for pop in getPopList():
+        list.append(make_numbers(pop));
+    #print(list)
+    return list
