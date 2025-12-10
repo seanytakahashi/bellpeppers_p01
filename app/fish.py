848f1aeb15a211c27fd5f3352f761acf7427029f
Ivan Chen <ichen60@stuy.edu>
2025-12-10 11:01:03 -0500

    update

--- app/fish.py
+++ app/fish.py
@@ -8,11 +8,20 @@ import utility
 import random
 
 def get_fish():
-    fishSet = utility.call_api("Species", "/export", {
-        "format": "json",
-        "columns": "/species@cn,sn,status,range_envelope,gn",
-        "sort": "/species@cn asc;/species@sn asc",
-        "filter": "/species@range_envelope is not null",
-        "filter": "/species@status not in ('Experimental Population, Non-Essential')"
-    })["data"]
+    fishSet = utility.call_api("Species", "/export", [
+        ("format", "json"),
+        ("columns", "/species@cn,sn,status,range_envelope,gn"),
+        ("sort", "/species@cn asc;/species@sn asc"),
+        ("filter", "/species@status not in ('Experimental Population, Non-Essential')"),
+        ("filter", "/species@range_envelope is not null")
+    ])["data"]
+    # x = 0
+    for fish in fishSet:
+        # x += 1
+        try:
+            fish[3] = utility.find_area(fish[3][9:-2])
+        except:
+            print(fish[0])
+            print(fish[3])
+    # print(x)
     return random.choice(fishSet)
--- app/utility.py
+++ app/utility.py
@@ -46,10 +46,10 @@ def call_api(api_name, path, params={}):
     return json.loads(data)
 
 def find_area(polygon):
-    sum1 = 0
     sum2 = 0
-    polygon.replace(",", " ")
-    set = polygon.split(" ")
+    sum1 = 0
+    polygon.replace(" ", ",")
+    set = polygon.split(",")
     pair1 = []
     pair2 = []
     firstpair = []
@@ -70,9 +70,9 @@ def find_area(polygon):
 # print(call_api("Dnd", "/equipment-categories/simple-weapons")["index"])
 
 # print(call_api("Species", "/export", {
-#  "format": "json",
-#  "columns": "/species@cn,sn,status,range_envelope,gn",
-#  "sort": "/species@cn asc;/species@sn asc",
-#  "filter": "/species@range_envelope is not null",
-#  "filter": "/species@status not in ('Experimental Population, Non-Essential')"
+#     "format": "json",
+#     "columns": "/species@cn,sn,status,range_envelope,gn",
+#     "sort": "/species@cn asc;/species@sn asc",
+#     "filter": "/species@range_envelope is not null",
+#     "filter": "/species@status not in ('Experimental Population, Non-Essential')"
 # })["data"][0])
