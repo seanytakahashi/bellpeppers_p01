cd4a791b2a88a047d684a47d47259f086514f1a9
Ivan Chen <ichen60@stuy.edu>
2025-12-10 10:36:33 -0500

    added getFish() method, returns a random fish

--- app/fish.py
+++ app/fish.py
@@ -3,3 +3,16 @@
 # SoftDev
 # P01
 # 2025-12-22m
+
+import utility
+import random
+
+def getFish():
+    fishSet = utility.call_api("Species", "/export", {
+        "format": "json",
+        "columns": "/species@cn,sn,status,range_envelope,gn",
+        "sort": "/species@cn asc;/species@sn asc",
+        "filter": "/species@range_envelope is not null",
+        "filter": "/species@status not in ('Experimental Population, Non-Essential')"
+    })["data"]
+    return random.choice(fishSet)
