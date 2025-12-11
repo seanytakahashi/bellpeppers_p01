689438f9e8f2a235bf244b9d2b254aba554a8898
James Lei <leijames325@gmail.com>
2025-12-11 10:24:31 -0500

    Removed db file

---
+++ app/cache.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+DB_FILE = "data.db"
+
+db = sqlite3.connect(DB_FILE)
+c = db.cursor()
+
+c.executescript("""
+DROP TABLE IF EXISTS profiles;
+CREATE TABLE profiles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT,
+    country TEXT DEFAULT 'USA',
+    balance INTEGER DEFAULT 0
+);""")
+
+c.executescript("""
+DROP TABLE IF EXISTS fish;
+CREATE TABLE fish (
+    scientific_name TEXT,
+    common_name TEXT,
+    owner INTEGER,
+    number_caught INTEGER,
+    number_owned INTEGER,
+    FOREIGN KEY (owner) REFERENCES profiles(id)
+);""")
+
+c.executescript("""
+DROP TABLE IF EXISTS entries;
+CREATE TABLE weapons (
+    name TEXT,
+    owner INTEGER,
+    durability INTEGER,
+    FOREIGN KEY (owner) REFERENCES profiles(id)
+);""")
+
+db.commit()
+db.close()
